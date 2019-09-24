f6580a7c6f1311fa2d9ec6f80da77425510f38de
Igor PRADELLA <i.pradella@evs.com>
2019-09-24 17:03:23 +0200

    add call to service

--- Front/src/pages/Home.js
+++ Front/src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Home.css';
+import axios from 'axios';
 
 class Home extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://localhost:3001/inside`)
+      .then(res => {
+        this.setState({ insideTemp: res });
+      })
+  }
 
 render() {
 
   return (
     <div className="Home">
         <div class="selector">
-          <button>18° Consigne</button>
+          <button>{this.State.insideTemp} Consigne</button>
         </div>
     </div>
   );
