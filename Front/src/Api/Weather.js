3f1a507b2c06b629510a492450c9603201edb95f
Igor PRADELLA <i.pradella@evs.com>
2019-10-16 14:01:52 +0200

    Monitor inside outside and desired temperature

--- Front/src/Api/Weather.js
+++ Front/src/Api/Weather.js
@@ -4,9 +4,9 @@ export default class Weather {
   static async getLocalWeather() {
     return axios.get('http://api.openweathermap.org/data/2.5/weather', {
       params: {
-        id: 'e1f43933f8230cdbf1c52aa71168aed2',
-        appid: '2803183',
-        units: 'metric'
+        appid: "e1f43933f8230cdbf1c52aa71168aed2",
+        id: "2803183",
+        units: "metric"
       }
     })
   }
--- Front/src/pages/Home.js
+++ Front/src/pages/Home.js
@@ -2,29 +2,34 @@ import React from 'react';
 import './Home.css';
 import Temperature from '../Api/Temperature';
 import Weather from '../Api/Weather';
+import tempOutImg from '../img/temp-out.png';
+import tempInImg from '../img/temp-in.png';
 
 class Home extends React.Component {
 
   state = {
     insideTemp: 'N/A',
     desiredTemp: 'N/A',
-    value: 0,
+    weather: {main:{temp:'N/A'}},
   }
 
   componentDidMount() {
-    this.setState({ 
-      insideTemp: Temperature.getInsideTemperature(),
-      desiredTemp: Temperature.getDesiredTemperature(),
-      weather: Weather.getLocalWeather()
-     });
+    Temperature.getInsideTemperature().then(res => this.setState({ insideTemp: res.data }));
+    Temperature.getDesiredTemperature().then(res => this.setState({ desiredTemp: res.data }));
+    Weather.getLocalWeather().then(res => this.setState({ weather: res.data }));
+  }
+
+  round(value) {
+    return Math.round(value*2)/2;
   }
 
 render() {
-  var {insideTemp} = this.state;
+  var {insideTemp, desiredTemp, weather } = this.state;
 
   return (
     <div class="Home">
       <div class="single-chart">
+      <p class="outside-text"><img src={tempInImg} /> {this.round(insideTemp)}°C</p> 
         <svg viewBox="0 0 36 36" class="circular-chart orange" xmlns="http://www.w3.org/2000/svg">
           <path class="circle-bg"
             d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
@@ -33,10 +38,10 @@ render() {
             stroke-dasharray="80, 100"
             d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"
           />
-          <text x="18" y="20.35" class="inside-text">{insideTemp}°C</text> 
+          <text x="18" y="20.35" class="inside-text">{this.round(desiredTemp)}°C</text> 
         </svg>
-        <img src="../img/temp-in.png" /> 
       </div>
+      <p class="outside-text"><img src={tempOutImg} /> {this.round(weather.main.temp)}°C</p> 
     </div>
   );
 }
